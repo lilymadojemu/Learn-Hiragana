24f734e2ac0aab504dba71e1219806ef5f9ab4cd
Lily <lmadojem@andrew.cmu.edu>
2021-11-12 11:39:24 -0500

    Update #Populate Values.py

--- #Populate Values.py
+++ #Populate Values.py
@@ -2,4 +2,13 @@
 
 '''
 Where I will dynamically populate word and character values 
+I want certain vocab to only appear after learning other values
+Do I want verbs or nouns？
 '''
+import os
+
+#Getting Hiragana & romanji
+
+
+
+#Getting JAPN Vocab & Romanji
