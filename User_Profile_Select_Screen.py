d7c39d22bb011520b3b6b4f540f7cec68bd385b3
Lily <lmadojem@andrew.cmu.edu>
2021-11-14 17:14:17 -0500

    Changing wording from intro to start

--- User_Profile_Select_Screen.py
+++ User_Profile_Select_Screen.py
@@ -1,1 +1,14 @@
-#User_Profile_Select_Screen
+def userSelect_mousePressed(app,event):
+    #Parameters for learning button to be pressed
+    #if event.y ya know...
+        app.phase = 'learning'
+
+#Initates Learning Mode
+def drawLetsLearnButton(app,canvas):
+    canvas.create_rectangle(app.cx,app.cy,app.cx//2,app.cy//2, fill = 'purple')
+    canvas.create_text(app.cx//2,app.cy//2,
+                        font = 'Arial',  text = "Let's learn!", fill = 'Red')
+
+def userProfileRedrawAll(app,canvas):
+    #will house anything drawn for the user Profile
+    drawLetsLearnButton(app,canvas)
