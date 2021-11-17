a5b87df34c0bff3c19882fbf0ee91f8d0fb4de1f
Lily <lmadojem@andrew.cmu.edu>
2021-11-17 17:20:49 -0500

    Base Learning Mode

--- Classes.py
+++ Classes.py
@@ -4,6 +4,7 @@
 Contains all classes that will be used
 
 '''
+from Populate_Values import*
 import os, random, time
 class correctWord(object):
     def __init__(self, correctWord):
@@ -14,9 +15,18 @@ class incorrectWord(object):
     def __init__(self, incorrectWord):
         self.incorrectWord = incorrectWord
 
+kanaCard = Flashcard(characterDictionary,characterDictionary[key])
+vocabCard = Flashcard(vocabularyDictionary, vocabularyDictionary[key])
 
 class FlashCard(object):
-    def __init__(self, app, canvas, frontText, backText):
+    def __init__(self, frontText, backText characterCardCount, vocabCardCount, 
+                    app, canvas,):
+        self.characterCardCount = characterCardCount
+        self.vocabCardCount = vocabCardCount
+
+
+        self.newFront = backText
+        self.newBack = frontText
         self.app = app
         self.canvas = canvas
         self.frontText = frontText
@@ -30,17 +40,28 @@ class FlashCard(object):
         self.canvas.create_text(self.app.cx//1.5,self.app.cy//3,font = 'Arial',
     text = f"KanaLevel:{self.app.characterLevel},{self.app.vocabLevel}", 
                             fill = 'medium aquamarine')
+        for key in characterDictionary:
+            self.frontText = key
+            self.backText = characterDictionary[key]
+            self.canvas.create_text(self.app.cx//1.5,self.app.cy//3,
+                            font = 'Arial',
+                            text = f"{self.frontText}", 
+                            fill = 'medium aquamarine')
+            self.canvas.create_text(self.app.cx,self.app.cy//3,font = 'Arial',
+                            text = f"{self.backText}", 
+                            fill = 'medium aquamarine')
+
     def getMeaning(self, word):
         if word in characterDictionary:
-            return characterDicitionary[word]
+            return characterDictionary[word]
         elif word in vocabularyDictionary:
-            return vocabularyDictionay[word]
+            return vocabularyDictionary[word]
         else:
             self.app.showMessage('Sorry, that word is invalid:(')
-    def isFlipped(self):
-        #if flashcard has been clicked
-        if isClicked:
-            isFlipped = not isFlipped
+    def flip(self):
+        if self.app.isFlipped == True:
+            self.frontText = self.newFront
+            self.backText = self.newBack
 
 
 
@@ -54,21 +75,28 @@ class FlashCard(object):
 
 #English Vocabulary subclass
 
+def Merge(dict1, dict2):
+    res = {**dict1, **dict2}
+    return res
 
+
+Problemtime - currTime 
 #bot that will hold all the inner processes for learner
 class SenseiBot(object):
-    def __init__(self,botName, possibleChoices):
+    def __init__(self,botName, possibleChoices, targetAnswer, app, currTime):
         self.botName = botName
+        self.possibleChoices = possibleChoices
+        self.targetAnswer = targetAnswer
+        self.app = app
+        self.currTime = currTime
         pass
     #Information regarding the choices a user will be able to 
     # select for each prob.
     #Mainy applies in Practice Phase
     def answerChoices(self):
-        #Possible choices is either a dictionary or a list, most likely a dictionary
-        def vocabChoices(self,selectedWord):
-            pass
-        def characterChoices(self,selectedWord):
-            pass
+        self.possibleChoices = Merge(self.characterDictionary, 
+                                    self.vocabularyDictionary)
+        random.random(self.possibleChoices)
         pass
     #Determines whether or not an answer is correct or not,
     #If correct, bot will say "correct!"
@@ -78,15 +106,17 @@ class SenseiBot(object):
     def isCorrect(self, answerChoice):
         correctMessages = ["That's Correct!", "You're the best!"]
         incorrectMessages = ["Sorry, that's incorrect","Better luck next time!"]
-        random.random(correctMessages)
-        random.random(incorrectMessages)
-        pass
-
+        if answerChoice == self.possibleChoice[self.targetAnswer]:
+            self.app.showMessage(random.random(correctMessages))
+        else:
+            self.app.showMessage(random.random(incorrectMessages))
     #How long user has to select/input answer
     #Should time vary depending on if already known or not
     #If varies, need to be based on Assigned time
     #Practice Phase
-    def InternalProblemTimer(self,time):
+    def InternalProblemTimer(self,app,time):
+        self.app = app
+        self.problemTime = 
         pass
 '''
 Each key-value pair, once answered will be assigned an initial time and a time
